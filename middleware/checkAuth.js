43fd1480def4cd1912e5c6a929ddb7971e864348
Jim <jim@advancebass.com>
2023-05-04 11:16:45 -0700

    fix middleware bug

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -2,7 +2,6 @@ module.exports = (req, res, next) => {
   const secret = 'neocat';
   if (req.query.api_key !== secret) {
     res.status(401).json({ message: 'not authorized' });
-    next();
   } else {
     next();
   }
