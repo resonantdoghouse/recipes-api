0f4bee7571cddb93bf2431ac9051fc3708e6ee1c
Jim <jim@advancebass.com>
2024-04-23 11:38:09 -0700

    change version number

--- swagger.js
+++ swagger.js
@@ -1,27 +1,28 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
-import swaggerAutogen from 'swagger-autogen';
+import swaggerAutogen from "swagger-autogen";
 
 let host, schemes;
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   host = `localhost:${process.env.PORT}`;
-  schemes = ['http'];
+  schemes = ["http"];
 } else {
-  ('yummy-recipes.herokuapp.com');
-  schemes = ['https'];
+  ("yummy-recipes.herokuapp.com");
+  schemes = ["https"];
 }
 
 const doc = {
   info: {
-    title: 'Recipes API',
-    description: 'A practice REST API with mouth-watering recipes?',
+    title: "Recipes API",
+    description: "A practice REST API with mouth-watering recipes?",
+    version: "1.0.1",
   },
   schemes,
   host,
 };
 
-const outputFile = './swagger_output.json';
-const endpointsFiles = ['./server.js'];
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["./server.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
